f8936bb92e2b29af7821924a5f05eeefe6efa55a
kora11e <87006301+kora11e@users.noreply.github.com>
2023-12-13 20:15:30 +0100

    update

---
+++ pipelines/train_pipeline.py
@@ -0,0 +1,12 @@
+from zenml import pipelines
+from steps.ingest import ingest_df
+from steps.clean_data import clean_df
+from steps.model_train import train_model
+from steps.evaluation import evaluate_model
+
+@pipelines(enale_cache = False)
+def training_pipeline(data_path: str):
+    df = ingest_df(data_path)
+    clean_df(df)
+    evaluate_model(df)
+
