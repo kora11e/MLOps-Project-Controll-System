2977ad30c617db6790af7780cb3fc95768f9b1c3
kora11e <87006301+kora11e@users.noreply.github.com>
2023-12-14 19:39:58 +0100

    update

--- run_deployment.py
+++ run_deployment.py
@@ -1,3 +1,4 @@
+from pipelines.deployment_pipeline import continuous_deployment_pipeline
 from pipelines.deployment_pipeline import deployment_pipeline, inference_pipeline
 import click
 
@@ -21,7 +22,25 @@ DEPLOY_AND_PREDICT = 'deploy and predict'
 )
 
 def run_deployment(config: str, min_accuracy: float):
+    mlflow_model_deployer_component = mlflow_model_deployer_component.get_active_model_deployer()
+    deploy = config == DEPLOY or config == DEPLOY_AND_PREDICT
+    predict = config == PREDICT or config == DEPLOY_AND_PREDICT
+
     if deploy:
-        deployment_pipeline(min_accuracy)
+        continuous_deployment_pipeline(
+            min_accuracy=min_accuracy,
+            workers=3,
+            timeout=60,
+        )
+        
     if predict:
         inference_pipeline()
+
+    if existing_services:
+        service = cast(MLFlowDeploymentService, existing_service[0])
+        if service.is_running:
+            print('The service is running')
+        elif service.is_failed:
+            print('An Error has occured')
+    else:
+        print('')
