7c55f03f53d6665d4a6659d2874e7e71c2d54256
kora11e <87006301+kora11e@users.noreply.github.com>
2023-12-14 19:01:37 +0100

    update

--- pipelines/train_pipeline.py
+++ pipelines/train_pipeline.py
@@ -7,6 +7,7 @@ from steps.evaluation import evaluate_model
 @pipelines(enale_cache = False)
 def training_pipeline(data_path: str):
     df = ingest_df(data_path)
-    clean_df(df)
-    evaluate_model(df)
+    X_train, X_test, y_train, y_test = clean_df(df)
+    model = train_model(X_train, X_test, y_train, y_test)
+    r2_score, mse = evaluate_model(model, X_test, y_test)
 
--- steps/evaluation.py
+++ steps/evaluation.py
@@ -1,7 +1,29 @@
+from typing import Tuple
+from typing_extensions import Annotated
 import logging 
 import pandas as pd
 from zenml import step
+from src.evaluate import MSE, R2
+from sklearn.base import RegressorMixin
 
 @step
-def evaluate_model(df: pd.DataFrame) -> None:
-    pass
+def evaluate_model(model: RegressorMixin,
+    X_test: pd.DataFrame,
+    y_test: pd.DataFrame,   
+    ) -> Tuple[
+        Annotated[float, 'r2_score'],
+        Annotated[float, 'mse']
+    ]:
+    
+    prediction = model.predict(X_test)
+    mse_class = MSE()
+    mse = mse_class.calculate_scores(y_test, prediction)
+
+    r2_class = R2()
+    r2 = r2_class.calculate_scores(y_test, prediction)
+
+    return r2_score, mse
+
+except Exception as e:
+    logging.error(f'Error: {e}')
+    raise e
